f834049ff9eef307fe09456c7692d9adc5b70066
perako2023 <135764815+perako2023@users.noreply.github.com>
2023-09-28 23:51:46 -0500

    feat(Logout): token validation

--- src/controllers/auth/logout.controller.ts
+++ src/controllers/auth/logout.controller.ts
@@ -1,7 +1,15 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 
 export default async function logout(req: express.Request, res: express.Response) {
 	try {
+		const { token } = req.cookies
+		if (!token) return res.sendStatus(401)
+
+		const isTokenValid = jwt.verify(token, process.env.JWT_SECRET!)
+		if (!isTokenValid) return res.sendStatus(401)
+
 		res.clearCookie('token')
 
 		return res.sendStatus(200)
